d27daeb34563af25a324de5810aa8b03efc90a8b
S A D D A M <S A D D A M@Windows10>
2018-09-13 12:53:11 +0700

    Tambah input

--- src/j07/J07.java
+++ src/j07/J07.java
@@ -9,11 +9,20 @@ public class J07 {
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
     }
+    private static int tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke-:");
+        int n = scanner.nextInt();
+        
+        return n;
+    }
     
     public static void main(String[] args) {
         String identitas = "Saddam Raihan Ramadhan / XR2 / 30";
         
         tampilJudul(identitas);
+        int n = tampilInput();
     }
     
 }
