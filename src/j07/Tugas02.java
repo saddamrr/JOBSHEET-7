b027c8698704cff76fc14fd11f0425877b799c4e
S A D D A M <S A D D A M@Windows10>
2018-09-14 21:16:30 +0700

    Initial Commit

---
+++ src/j07/Tugas02.java
@@ -0,0 +1,10 @@
+package j07;
+import java.util.Scanner;
+public class Tugas02 {
+    public static void main(String[] args) {
+        System.out.println("NAMA : SADDAM RAIHAN RAMADHAN");
+        System.out.println("KELAS: XRPL 2    NO.ABSEN : 30");
+        System.out.println("");
+        
+    }
+}
