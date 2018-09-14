aa33b7f219a4b0ce4846b01c15100b36933c97e1
S A D D A M <S A D D A M@Windows10>
2018-09-14 21:17:08 +0700

    Tambah Variabel

--- src/j07/Tugas02.java
+++ src/j07/Tugas02.java
@@ -6,5 +6,9 @@ public class Tugas02 {
         System.out.println("KELAS: XRPL 2    NO.ABSEN : 30");
         System.out.println("");
         
+        Scanner sistem = new Scanner(System.in);
+        int arr[]= {7, 5, 4, 3, 2};
+        int arr1[]= {10000, 7000, 5000, 3000, 2000};
+        int bayaran = 0;
     }
 }
